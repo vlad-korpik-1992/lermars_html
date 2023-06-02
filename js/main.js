971ab4b3a0e76dfe0bce683b950df9b4c9f4bf01
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-06-02 14:20:25 +0300

    Desktop version of the home page is ready

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.about__box__column__items').on('click', function (e) {
+        e.preventDefault();
+        let elem = e.target;
+		let id = '1' + elem.getAttribute('id');
+        $('.about__box__item__inner').removeClass('about__box__item__inner--active');
+		jQuery("#"+id).addClass('about__box__item__inner--active');	
+    });
+    $('.services__box').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		rows: 1,
+		arrows: true,
+		dots: false,
+		autoplay: false,
+		infinite: false
+	});
+});
